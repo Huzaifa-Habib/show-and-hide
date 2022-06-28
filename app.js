285a2cd5727637708d874c1a05ed784427df4545
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-28 23:30:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var content =document.getElementById("content");
+var button  =document.getElementById("show");
+var hide = document.getElementById("hide")
+
+
+button.onclick= function () {
+    content.style.display="block"
+   
+    
+}
+
+hide.onclick= function () {
+    content.style.display="none"
+   
+    
+}
+
+
